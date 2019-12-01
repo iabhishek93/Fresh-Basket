d61444d612601c06b50b61472437569b74b3eab3
iabhishek93 <abhishekbhowmick03@gmail.com>
2019-12-02 00:10:26 +0530

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCQwgXte3JXiZHtL-zYyymRfIWdTy3rUp0",
-    authDomain: "oshop-3321d.firebaseapp.com",
-    databaseURL: "https://oshop-3321d.firebaseio.com",
-    projectId: "oshop-3321d",
-    storageBucket: "",
-    messagingSenderId: "70556684454",
-    appId: "1:70556684454:web:e6552865ffa58959"
+    apiKey: "AIzaSyBlSO2TI-mg_yhxosCB1SxtM8lso6eK_Wo",
+    authDomain: "friendlychat-b25ce.firebaseapp.com",
+    databaseURL: "https://friendlychat-b25ce.firebaseio.com",
+    projectId: "friendlychat-b25ce",
+    storageBucket: "friendlychat-b25ce.appspot.com",
+    messagingSenderId: "1029858406083",
+    appId: "1:1029858406083:web:76beac18e531536f"
   }
 };
